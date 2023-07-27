9d73c6bc0d4056739717753717150b3571503ae1
isaacokoroafor <isaacokoroafor@icloud.com>
2023-07-27 14:19:22 +0100

    menu section

---
+++ js/app.js
@@ -0,0 +1,19 @@
+new Swiper("#swiper-1", {
+  effect: "fade",
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: "#swiper-1 .swiper-pagination",
+    clickable: true,
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: "#nav-right",
+    prevEl: "#nav-left",
+  },
+});
