70c8c16c2e7cc70d9dbd50643177e993b9d86fe2
isaacokoroafor <isaacokoroafor@icloud.com>
2023-08-01 02:42:02 +0100

    location modal 1/2

--- js/app.js
+++ js/app.js
@@ -19,3 +19,14 @@ new Swiper("#swiper-1", {
   preventClicks: false,
   preventClicksPropagation: false,
 });
+
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
